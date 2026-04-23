87622c0292dcf7e418fb034b5d1157b6f08e1528
Camila Rosero <mcamilarh26@gmail.com>
2026-04-23 10:31:22 -0500

    Cambios generales

--- src/main/java/com/epq/epqbackend/model/ArchivoExcel.java
+++ src/main/java/com/epq/epqbackend/model/ArchivoExcel.java
@@ -1,13 +1,20 @@
 package com.epq.epqbackend.model;
 
+import jakarta.persistence.*;
 import java.time.LocalDate;
 
+@Entity
 public class ArchivoExcel {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nombreArchivo;
-    private String ruta;
+
     private Long peso;
+
     private LocalDate fechaCarga;
 
+    private String tipo;
 }
